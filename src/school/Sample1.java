fece51065e9dcd54ac5eab25ba24b8fdca19a4b4
kazu2377 <jtths474@gmail.com>
2024-11-21 17:24:37 +0900

    山梨と修正した

--- src/school/Sample1.java
+++ src/school/Sample1.java
@@ -3,7 +3,7 @@ package school;
 public class Sample1 {
 
 	public static void main(String[] args) {
-		System.out.println("はろーZ　新潟");
+		System.out.println("はろーZ 山梨");
 		System.out.println();
 	}
 
