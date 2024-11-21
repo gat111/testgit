5e5417db4860ff8a38ad60b213d1c06c3e4b37f4
HI <141301784+gat111@users.noreply.github.com>
2024-11-21 17:25:19 +0900

    再度修正

--- src/school/Sample1.java
+++ src/school/Sample1.java
@@ -3,7 +3,7 @@ package school;
 public class Sample1 {
 
 	public static void main(String[] args) {
-		System.out.println("はろーZ　新潟");
+		System.out.println("はろーZ　新潟東京と");
 		System.out.println();
 	}
 
