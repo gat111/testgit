3266c70a7d8df6e2c6868bed8631e624873c66a6
HI <141301784+gat111@users.noreply.github.com>
2024-11-27 18:48:24 +0900

    ぷるてすと

--- src/school/Sample3.java
+++ src/school/Sample3.java
@@ -5,7 +5,7 @@ public class Sample3 {
 	public static void main(String[] args) {
 
 
-		System.out.println("はろーZ　新潟東京と　大阪　アメリカ　カナダ");
+		System.out.println("はろーZ　新潟東京と　大阪　アメリカ　カナダ 東京");
 		System.out.println();
 	}
 
